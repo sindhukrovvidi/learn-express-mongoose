74a878c9f43cc46ccd48bb0d049638fcaef6d324
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-03-15 17:43:16 -0400

    March 12 Activity

--- models/author.js
+++ models/author.js
@@ -1,34 +1,41 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var AuthorSchema = new Schema(
-  {
-    first_name: {type: String, required: true, maxLength: 100},
-    family_name: {type: String, required: true, maxLength: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
-  }
-);
+var AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
 
 // Virtual for author's full name
-AuthorSchema
-.virtual('name')
-.get(function () {
-// To avoid errors in cases where an author does not have either a family name or first name
-// We want to make sure we handle the exception by returning an empty string for that case
-  var fullname = '';
+AuthorSchema.virtual("name").get(function () {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  var fullname = "";
   if (this.first_name && this.family_name) {
-    fullname = this.family_name + ', ' + this.first_name
+    fullname = this.family_name + ", " + this.first_name;
   }
   if (!this.first_name || !this.family_name) {
-    fullname = '';
+    fullname = "";
   }
   return fullname;
 });
 
 // Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {});
+AuthorSchema.virtual("lifespan").get(function () {
+  var lifespan_str = "";
+  if (this.date_of_birth) {
+    lifespan_str = this.date_of_birth.getFullYear().toString();
+  }
+  lifespan_str += "-";
+  if (this.date_of_death) {
+    lifespan_str += this.date_of_death.getFullYear().toString();
+  }
+
+  return lifespan_str;
+});
 
 //Export model
-module.exports = mongoose.model('Author', AuthorSchema);
+module.exports = mongoose.model("Author", AuthorSchema);
