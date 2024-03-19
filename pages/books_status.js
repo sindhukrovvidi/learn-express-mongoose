a5c25e24264455d262d6a430b3b909b664193b79
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-03-19 18:56:25 -0400

    Class activity

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,13 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  BookInstance.find({status: 'Available'})
+    .populate('book')
+    .exec()
+    .then(list_bookinstances => {
+      res.send(list_bookinstances.map(function(bookinstance){
+        return BookInstance.book.title + " : " + BookInstance.status;
+      }));
+    })
+    .catch(err => {res.send('Status not found ')});
 }
